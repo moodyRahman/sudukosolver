112b280c39a2bf152acdc502c9a0fe17220a1fd1
Moody Rahman <mrahman01@stuy.edu>
2019-11-01 09:46:22 -0400

    OBJECT ORIENTED GANG

--- naiverecurse.py
+++ naiverecurse.py
@@ -87,42 +87,17 @@ _,_,7,_,_,_,_,_,_
 _,8,_,_,_,4,_,6,_
 _,2,_,_,_,_,_,_,_"""
 
-CURRENT_BOARD = a5
+CURRENT_BOARD = a5a7 = """_,_,_,2,_,_,_,_,_
+_,_,_,_,9,_,5,_,_
+1,_,6,_,_,_,_,4,_
+_,_,_,_,_,_,_,8,_
+4,_,_,_,_,6,_,_,_
+_,9,_,3,_,_,2,_,_
+_,_,7,_,_,_,_,_,_
+_,8,_,_,_,4,_,6,_
+_,2,_,_,_,_,_,_,_"""
 
 # "7, 6, 2"
-
-cliques = [[0, 1, 2, 3, 4, 5, 6, 7, 8],
-           [9, 10, 11, 12, 13, 14, 15, 16, 17],
-           [18, 19, 20, 21, 22, 23, 24, 25, 26],
-           [27, 28, 29, 30, 31, 32, 33, 34, 35],
-           [36, 37, 38, 39, 40, 41, 42, 43, 44],
-           [45, 46, 47, 48, 49, 50, 51, 52, 53],
-           [54, 55, 56, 57, 58, 59, 60, 61, 62],
-           [63, 64, 65, 66, 67, 68, 69, 70, 71],
-           [72, 73, 74, 75, 76, 77, 78, 79, 80],
-           [0, 9, 18, 27, 36, 45, 54, 63, 72],
-           [1, 10, 19, 28, 37, 46, 55, 64, 73],
-           [2, 11, 20, 29, 38, 47, 56, 65, 74],
-           [3, 12, 21, 30, 39, 48, 57, 66, 75],
-           [4, 13, 22, 31, 40, 49, 58, 67, 76],
-           [5, 14, 23, 32, 41, 50, 59, 68, 77],
-           [6, 15, 24, 33, 42, 51, 60, 69, 78],
-           [7, 16, 25, 34, 43, 52, 61, 70, 79],
-           [8, 17, 26, 35, 44, 53, 62, 71, 80],
-           [0, 1, 2, 9, 10, 11, 18, 19, 20],
-           [3, 4, 5, 12, 13, 14, 21, 22, 23],
-           [6, 7, 8, 15, 16, 17, 24, 25, 26],
-           [27, 28, 29, 36, 37, 38, 45, 46, 47],
-           [30, 31, 32, 39, 40, 41, 48, 49, 50],
-           [33, 34, 35, 42, 43, 44, 51, 52, 53],
-           [54, 55, 56, 63, 64, 65, 72, 73, 74],
-           [57, 58, 59, 66, 67, 68, 75, 76, 77],
-           [60, 61, 62, 69, 70, 71, 78, 79, 80]
-           ]
-
-allnodes = []
-blanknodes = []
-
 class Node(object):
     """holds a node of the sudoku Node"""
 
@@ -134,92 +109,140 @@ class Node(object):
     def __repr__(self):
         return str([self.data, self.id])
 
+class Board(object):
+    """holds a sudoku Board"""
+
+    def __init__(self, arg):
+        super(Board, self).__init__()
+        self.CURRENT_BOARD = arg
 
 
-def calcAllNodes():
-    id = 0
-    for x in CURRENT_BOARD.split("\n"):
-        for z in x.split(","):
-            allnodes.append(Node(z, id))
-            id = id + 1
+    cliques = [[0, 1, 2, 3, 4, 5, 6, 7, 8],
+               [9, 10, 11, 12, 13, 14, 15, 16, 17],
+               [18, 19, 20, 21, 22, 23, 24, 25, 26],
+               [27, 28, 29, 30, 31, 32, 33, 34, 35],
+               [36, 37, 38, 39, 40, 41, 42, 43, 44],
+               [45, 46, 47, 48, 49, 50, 51, 52, 53],
+               [54, 55, 56, 57, 58, 59, 60, 61, 62],
+               [63, 64, 65, 66, 67, 68, 69, 70, 71],
+               [72, 73, 74, 75, 76, 77, 78, 79, 80],
+               [0, 9, 18, 27, 36, 45, 54, 63, 72],
+               [1, 10, 19, 28, 37, 46, 55, 64, 73],
+               [2, 11, 20, 29, 38, 47, 56, 65, 74],
+               [3, 12, 21, 30, 39, 48, 57, 66, 75],
+               [4, 13, 22, 31, 40, 49, 58, 67, 76],
+               [5, 14, 23, 32, 41, 50, 59, 68, 77],
+               [6, 15, 24, 33, 42, 51, 60, 69, 78],
+               [7, 16, 25, 34, 43, 52, 61, 70, 79],
+               [8, 17, 26, 35, 44, 53, 62, 71, 80],
+               [0, 1, 2, 9, 10, 11, 18, 19, 20],
+               [3, 4, 5, 12, 13, 14, 21, 22, 23],
+               [6, 7, 8, 15, 16, 17, 24, 25, 26],
+               [27, 28, 29, 36, 37, 38, 45, 46, 47],
+               [30, 31, 32, 39, 40, 41, 48, 49, 50],
+               [33, 34, 35, 42, 43, 44, 51, 52, 53],
+               [54, 55, 56, 63, 64, 65, 72, 73, 74],
+               [57, 58, 59, 66, 67, 68, 75, 76, 77],
+               [60, 61, 62, 69, 70, 71, 78, 79, 80]
+               ]
 
-# print(allnodes)
+    allnodes = []
+    blanknodes = []
 
 
-def calcBlankNodes():
-    for x in allnodes:
-        if x.data == '_':
-            blanknodes.append(x)
-
-calcAllNodes()
-calcBlankNodes()
-
-# void findSolutions(n, other params) :
-#     if (found a solution) :
-#         solutionsFound = solutionsFound + 1;
-#         displaySolution();
-#         if (solutionsFound >= solutionTarget) :
-#             System.exit(0);
-#         return
-#
-#     for (val = first to last) :
-#         if (isValid(val, n)) :
-#             applyValue(val, n);
-#             findSolutions(n+1, other params);
-#             removeValue(val, n);
 
 
-def findSolutions(nodeindex):
-    if (isSolved(allnodes)):
-        return True
-    else:
-        nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-        if nodeindex < len(blanknodes):
-            relevantcliques = []
-            for clique in cliques:
-                if blanknodes[nodeindex].id in clique:
-                    relevantcliques.append(clique)
-            # print()
-            # print("I AM NODE", blanknodes[nodeindex].id)
-            # print(relevantcliques)
 
-            for clique in relevantcliques:
-                for x in clique:
-                    if allnodes[x].data in nums:
-                        nums.remove(allnodes[x].data)
+    def calcAllNodes(self):
+        id = 0
+        for x in CURRENT_BOARD.split("\n"):
+            for z in x.split(","):
+                self.allnodes.append(Node(z, id))
+                id = id + 1
 
-        # print(nums)
+    # print(allnodes)
 
-        for x in nums:
-            if nodeindex < len(blanknodes):
-                blanknodes[nodeindex].data = x
-                if (findSolutions(nodeindex + 1)):
-                    return True
+
+    def calcBlankNodes(self):
+        for x in self.allnodes:
+            if x.data == '_':
+                self.blanknodes.append(x)
+
+    # void findSolutions(n, other params) :
+    #     if (found a solution) :
+    #         solutionsFound = solutionsFound + 1;
+    #         displaySolution();
+    #         if (solutionsFound >= solutionTarget) :
+    #             System.exit(0);
+    #         return
+    #
+    #     for (val = first to last) :
+    #         if (isValid(val, n)) :
+    #             applyValue(val, n);
+    #             findSolutions(n+1, other params);
+    #             removeValue(val, n);
+
+
+    def findSolutions(self, nodeindex):
+        if (self.isSolved(self.allnodes)):
+            return True
+        else:
+            nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+            if nodeindex < len(self.blanknodes):
+                relevantcliques = []
+                for clique in self.cliques:
+                    if self.blanknodes[nodeindex].id in clique:
+                        relevantcliques.append(clique)
+                # print()
+                # print("I AM NODE", blanknodes[nodeindex].id)
+                # print(relevantcliques)
+
+                for clique in relevantcliques:
+                    for x in clique:
+                        if self.allnodes[x].data in nums:
+                            nums.remove(self.allnodes[x].data)
+
+            # print(nums)
+
+            for x in nums:
+                if nodeindex < len(self.blanknodes):
+                    self.blanknodes[nodeindex].data = x
+                    if (self.findSolutions(nodeindex + 1)):
+                        return True
+                    else:
+                        self.blanknodes[nodeindex].data = '_'
                 else:
-                    blanknodes[nodeindex].data = '_'
-            else:
+                    return False
+
+
+    def isSolved(self, in_nodes):
+        for x in self.cliques:
+            nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+            for y in x:
+                if self.allnodes[y].data in nums:
+                    nums.remove(self.allnodes[y].data)
+            if (len(nums) > 0):
                 return False
+        return True
 
 
-def isSolved(in_nodes):
-    for x in cliques:
-        nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-        for y in x:
-            if allnodes[y].data in nums:
-                nums.remove(allnodes[y].data)
-        if (len(nums) > 0):
-            return False
-    return True
+    # findSolutions(0)
+    # print("--- %s seconds ---" % (time.time() - start_time))
+
+    def printer(self):
+        for x in self.allnodes:
+            print(x.data, end = "")
+            if (x.id + 1) % 9 == 0:
+                print()
 
 
-findSolutions(0)
-print("--- %s seconds ---" % (time.time() - start_time))
-
-def printer():
-    for x in allnodes:
-        print(x.data, end = "")
-        if (x.id + 1) % 9 == 0:
-            print()
+    def solve(self):
+        self.calcAllNodes()
+        self.calcBlankNodes()
+        self.findSolutions(0)
+        soard.printer()
 
 
-printer()
+
+soard = Board(inboard)
+soard.solve()
